5e26739cf7a59f131441505b5aee5511ddbc9c08
Romain Trotard <romain.trotard@gmail.com>
2023-12-01 22:49:00 +0100

    Day1 part 2

--- day1/day1.go
+++ day1/day1.go
@@ -8,7 +8,35 @@ import (
 	"path/filepath"
 	"runtime"
 	"strconv"
+	"strings"
+    "slices"
 )
+
+var numberInLetterToNumberString = map[string]int{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+}
+
+func getNumberInLetterToNumberStringKeys() []string {
+	numberInLetterToNumberStringKeys := make([]string, 0, len(numberInLetterToNumberString))
+
+	for k := range numberInLetterToNumberString {
+		numberInLetterToNumberStringKeys = append(numberInLetterToNumberStringKeys, k)
+
+	}
+
+	return numberInLetterToNumberStringKeys
+}
+
+var numberInLetterToNumberStringKeys = getNumberInLetterToNumberStringKeys()
+
 
 func getFilePath(fileName string) string {
 	_, filename, _, ok := runtime.Caller(0)
@@ -50,21 +78,42 @@ func main() {
 
 	for fileScanner.Scan() {
 		line := fileScanner.Text()
-		var values []string
+		var values []int
+		consecutiveLetters := ""
 
 		for _, unicode := range line {
 			char := string(unicode)
 
-            // Not a number let's go next
-			if !isNumber(char) {
+			number, err := strconv.Atoi(char)
+
+			if err != nil {
+				consecutiveLetters += char
+
+				index := slices.IndexFunc(numberInLetterToNumberStringKeys, func(v string) bool {
+					return strings.Contains(consecutiveLetters, v)
+				})
+
+                // Does not correspond to a number in string
+				if index == -1 {
+					continue
+				}
+
+				values = append(values, numberInLetterToNumberString[numberInLetterToNumberStringKeys[index]])
+
+                // Reset the value because there was a match
+				consecutiveLetters = ""
 				continue
 			}
 
-			values = append(values, char)
+            // Let's clear consecutive letters because we have a number
+			consecutiveLetters = ""
+
+			values = append(values, number)
 		}
 
-        // Let's get the first and second value that constitute the final number of the line
-		var firstInt, secondInt string
+
+		// Let's get the first and second value that constitute the final number of the line
+		var firstInt, secondInt int
 
 		switch numberInLines := len(values); {
 		case numberInLines > 1:
@@ -75,10 +124,10 @@ func main() {
 			secondInt = values[0]
 		}
 
-        // Convert it
-		number, _ := strconv.Atoi(firstInt + secondInt)
+		// Convert it
+		number, _ := strconv.Atoi(strconv.Itoa(firstInt) + strconv.Itoa(secondInt))
 
-        // Add it to the final count
+		// Add it to the final count
 		count += number
 	}
 
