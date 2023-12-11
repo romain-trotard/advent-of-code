a847a76eca6ff67b90d7919aa986f301bc86d2f0
Romain Trotard <romain.trotard@gmail.com>
2023-12-11 19:28:23 +0100

    Day10 part 2

--- day10/day10.go
+++ day10/day10.go
@@ -35,9 +35,9 @@ func (point Point) canGoTop(tiles map[Point]TileWithMovement, countByPoint map[P
 		return false
 	}
 
-    value, exist := countByPoint[point.getTopPoint()]
+	_, exist = countByPoint[point.getTopPoint()]
 
-	if exist && value < nextIterationNumber {
+	if exist {
 		return false
 	}
 
@@ -56,9 +56,9 @@ func (point Point) canGoBottom(tiles map[Point]TileWithMovement, countByPoint ma
 		return false
 	}
 
-    value, exist := countByPoint[point.getBottomPoint()]
+	_, exist = countByPoint[point.getBottomPoint()]
 
-	if exist && value < nextIterationNumber {
+	if exist {
 		return false
 	}
 
@@ -77,9 +77,9 @@ func (point Point) canGoLeft(tiles map[Point]TileWithMovement, countByPoint map[
 		return false
 	}
 
-    value, exist := countByPoint[point.getLeftPoint()]
+	_, exist = countByPoint[point.getLeftPoint()]
 
-	if exist && value < nextIterationNumber {
+	if exist {
 		return false
 	}
 
@@ -98,9 +98,9 @@ func (point Point) canGoRight(tiles map[Point]TileWithMovement, countByPoint map
 		return false
 	}
 
-    value, exist := countByPoint[point.getRightPoint()]
+	_, exist = countByPoint[point.getRightPoint()]
 
-	if exist && value < nextIterationNumber {
+	if exist {
 		return false
 	}
 
@@ -124,7 +124,9 @@ type TileWithMovement interface {
 	canGoBottom() bool
 }
 
-type PipeValue struct{}
+type PipeValue struct {
+	direction string
+}
 
 func (value PipeValue) canGoRight() bool {
 	return false
@@ -137,6 +139,12 @@ func (value PipeValue) canGoTop() bool {
 }
 func (value PipeValue) canGoBottom() bool {
 	return true
+}
+func (value PipeValue) String() string {
+	if value.direction == "" {
+		return "|"
+	}
+	return value.direction
 }
 
 type DashValue struct{}
@@ -153,8 +161,13 @@ func (value DashValue) canGoTop() bool {
 func (value DashValue) canGoBottom() bool {
 	return false
 }
+func (value DashValue) String() string {
+	return "-"
+}
 
-type JValue struct{}
+type JValue struct {
+	direction string
+}
 
 func (value JValue) canGoRight() bool {
 	return false
@@ -168,8 +181,16 @@ func (value JValue) canGoTop() bool {
 func (value JValue) canGoBottom() bool {
 	return false
 }
+func (value JValue) String() string {
+	if value.direction == "" {
+		return "J"
+	}
+	return value.direction
+}
 
-type LValue struct{}
+type LValue struct {
+	direction string
+}
 
 func (value LValue) canGoRight() bool {
 	return true
@@ -183,8 +204,16 @@ func (value LValue) canGoTop() bool {
 func (value LValue) canGoBottom() bool {
 	return false
 }
+func (value LValue) String() string {
+	if value.direction == "" {
+		return "L"
+	}
+	return value.direction
+}
 
-type FValue struct{}
+type FValue struct {
+	direction string
+}
 
 func (value FValue) canGoRight() bool {
 	return true
@@ -198,8 +227,16 @@ func (value FValue) canGoTop() bool {
 func (value FValue) canGoBottom() bool {
 	return true
 }
+func (value FValue) String() string {
+	if value.direction == "" {
+		return "F"
+	}
+	return value.direction
+}
 
-type SevenValue struct{}
+type SevenValue struct {
+	direction string
+}
 
 func (value SevenValue) canGoRight() bool {
 	return false
@@ -212,6 +249,12 @@ func (value SevenValue) canGoTop() bool {
 }
 func (value SevenValue) canGoBottom() bool {
 	return true
+}
+func (value SevenValue) String() string {
+	if value.direction == "" {
+		return "7"
+	}
+	return value.direction
 }
 
 type StartingValue struct{}
@@ -228,6 +271,9 @@ func (value StartingValue) canGoTop() bool {
 func (value StartingValue) canGoBottom() bool {
 	return true
 }
+func (value StartingValue) String() string {
+	return "S"
+}
 
 type NoMovementValue struct{}
 
@@ -242,6 +288,9 @@ func (value NoMovementValue) canGoTop() bool {
 }
 func (value NoMovementValue) canGoBottom() bool {
 	return false
+}
+func (value NoMovementValue) String() string {
+	return "."
 }
 
 func getTileWithMovement(value string) TileWithMovement {
@@ -265,7 +314,7 @@ func getTileWithMovement(value string) TileWithMovement {
 	}
 }
 
-func travelImpl(iterationNumber int, startingPoint Point, currentPoint Point, tiles map[Point]TileWithMovement, distanceByPoint *map[Point]int) {
+func travelImpl(iterationNumber int, startingPoint Point, previousPoint Point, currentPoint Point, tiles *map[Point]TileWithMovement, distanceByPoint *map[Point]int) {
 	if iterationNumber != 0 && startingPoint == currentPoint {
 		return
 	}
@@ -274,36 +323,90 @@ func travelImpl(iterationNumber int, startingPoint Point, currentPoint Point, ti
 		(*distanceByPoint)[currentPoint] = iterationNumber
 	}
 
+	xDifference := currentPoint.x - previousPoint.x
+
+	// Actually I don't really know why JValue and LValue does not have anything
+	// Could have done the same with FValue and SevenValue
+	switch (*tiles)[currentPoint].(type) {
+	case PipeValue:
+		newValue := PipeValue{}
+		if xDifference == -1 {
+			newValue.direction = "U"
+		} else {
+			newValue.direction = "D"
+		}
+
+		(*tiles)[currentPoint] = newValue
+	case JValue:
+		newValue := JValue{}
+		if xDifference == 1 {
+			// newValue.direction = "D"
+		} else {
+			// newValue.direction = "U"
+		}
+
+		(*tiles)[currentPoint] = newValue
+	case SevenValue:
+		newValue := SevenValue{}
+		if xDifference == -1 {
+			newValue.direction = "U"
+		} else {
+			newValue.direction = "D"
+		}
+
+		(*tiles)[currentPoint] = newValue
+	case LValue:
+		newValue := LValue{}
+		if xDifference == 1 {
+			// newValue.direction = "D"
+		} else {
+			// newValue.direction = "U"
+		}
+
+		(*tiles)[currentPoint] = newValue
+	case FValue:
+		newValue := FValue{}
+		if xDifference == -1 {
+			newValue.direction = "U"
+		} else {
+			newValue.direction = "D"
+		}
+
+		(*tiles)[currentPoint] = newValue
+	default:
+	}
+
 	nextIterationNumber := iterationNumber + 1
 
-
-	if currentPoint.canGoTop(tiles, *distanceByPoint, nextIterationNumber) {
-		travelImpl(nextIterationNumber, startingPoint, currentPoint.getTopPoint(), tiles, distanceByPoint)
+	if currentPoint.canGoTop(*tiles, *distanceByPoint, nextIterationNumber) {
+		travelImpl(nextIterationNumber, startingPoint, currentPoint, currentPoint.getTopPoint(), tiles, distanceByPoint)
 	}
 
-	if currentPoint.canGoBottom(tiles, *distanceByPoint, nextIterationNumber) {
-		travelImpl(nextIterationNumber, startingPoint, currentPoint.getBottomPoint(), tiles, distanceByPoint)
+	if currentPoint.canGoBottom(*tiles, *distanceByPoint, nextIterationNumber) {
+		travelImpl(nextIterationNumber, startingPoint, currentPoint, currentPoint.getBottomPoint(), tiles, distanceByPoint)
 	}
 
-	if currentPoint.canGoLeft(tiles, *distanceByPoint, nextIterationNumber) {
-		travelImpl(nextIterationNumber, startingPoint, currentPoint.getLeftPoint(), tiles, distanceByPoint)
+	if currentPoint.canGoLeft(*tiles, *distanceByPoint, nextIterationNumber) {
+		travelImpl(nextIterationNumber, startingPoint, currentPoint, currentPoint.getLeftPoint(), tiles, distanceByPoint)
 	}
 
-	if currentPoint.canGoRight(tiles, *distanceByPoint, nextIterationNumber) {
-		travelImpl(nextIterationNumber, startingPoint, currentPoint.getRightPoint(), tiles, distanceByPoint)
+	if currentPoint.canGoRight(*tiles, *distanceByPoint, nextIterationNumber) {
+		travelImpl(nextIterationNumber, startingPoint, currentPoint, currentPoint.getRightPoint(), tiles, distanceByPoint)
 	}
 }
 
-func travel(point Point, tiles map[Point]TileWithMovement, distanceByPoint *map[Point]int) {
-	travelImpl(0, point, point, tiles, distanceByPoint)
+func travel(point Point, tiles *map[Point]TileWithMovement, distanceByPoint *map[Point]int) {
+	travelImpl(0, point, point, point, tiles, distanceByPoint)
 }
 
 func main() {
 	game := Game{tiles: map[Point]TileWithMovement{}}
 	lineNumber := 0
+	width := 0
 
 	utils.ForEachFileLine("day10/input.txt", func(line string) {
 		tiles := []string{}
+		width = len(line)
 
 		for y, unicode := range line {
 			char := string(unicode)
@@ -322,20 +425,102 @@ func main() {
 		lineNumber++
 	})
 
+	height := lineNumber
 
+	// Change starting point with right tile
 	distanceByPoint := &map[Point]int{}
 
-	travel(game.startingPoint, game.tiles, distanceByPoint)
+	startingPoint := game.startingPoint
+	if startingPoint.canGoBottom(game.tiles, *distanceByPoint, 0) && startingPoint.canGoTop(game.tiles, *distanceByPoint, 0) {
+		game.tiles[startingPoint] = PipeValue{}
+	} else if startingPoint.canGoLeft(game.tiles, *distanceByPoint, 0) && startingPoint.canGoRight(game.tiles, *distanceByPoint, 0) {
+		game.tiles[startingPoint] = DashValue{}
+	} else if startingPoint.canGoTop(game.tiles, *distanceByPoint, 0) && startingPoint.canGoRight(game.tiles, *distanceByPoint, 0) {
+		game.tiles[startingPoint] = LValue{}
+	} else if startingPoint.canGoBottom(game.tiles, *distanceByPoint, 0) && startingPoint.canGoRight(game.tiles, *distanceByPoint, 0) {
+		game.tiles[startingPoint] = SevenValue{}
+	} else if startingPoint.canGoTop(game.tiles, *distanceByPoint, 0) && startingPoint.canGoLeft(game.tiles, *distanceByPoint, 0) {
+		game.tiles[startingPoint] = JValue{}
+	} else if startingPoint.canGoBottom(game.tiles, *distanceByPoint, 0) && startingPoint.canGoRight(game.tiles, *distanceByPoint, 0) {
+		game.tiles[startingPoint] = FValue{}
+	}
 
+	travel(game.startingPoint, &game.tiles, distanceByPoint)
 
-    maxValue := 0
+	// Remove symbol that are useless
+	for point := range game.tiles {
+		_, exist := (*distanceByPoint)[point]
 
-    for _, value := range *distanceByPoint {
-        if maxValue < value {
-            maxValue = value
-        }
-    }
+		if !exist && point != game.startingPoint {
+			game.tiles[point] = NoMovementValue{}
+		}
+	}
 
+	for i := 0; i < height; i++ {
+		for j := 0; j < width; j++ {
+			point := Point{x: i, y: j}
 
-	fmt.Println("game", maxValue)
+			fmt.Print(game.tiles[point])
+		}
+		fmt.Println("")
+	}
+
+	count := 0
+	upDownCount := 0
+
+	for i := 0; i < height; i++ {
+		upDownCount = 0
+		for j := 0; j < width; j++ {
+			point := Point{x: i, y: j}
+
+			tile := game.tiles[point]
+
+			switch v := tile.(type) {
+			case PipeValue:
+				switch v.direction {
+				case "D":
+					upDownCount--
+				case "U":
+					upDownCount++
+				}
+			case LValue:
+				switch v.direction {
+				case "D":
+					upDownCount--
+				case "U":
+					upDownCount++
+				}
+			case JValue:
+				switch v.direction {
+				case "D":
+					upDownCount--
+				case "U":
+					upDownCount++
+				}
+			case SevenValue:
+				switch v.direction {
+				case "D":
+					upDownCount--
+				case "U":
+					upDownCount++
+				}
+			case FValue:
+				switch v.direction {
+				case "D":
+					upDownCount--
+				case "U":
+					upDownCount++
+				}
+			}
+
+			_, ok := tile.(NoMovementValue)
+
+			if ok && upDownCount != 0 {
+				count++
+			}
+
+		}
+	}
+
+	fmt.Println("Result", count)
 }
