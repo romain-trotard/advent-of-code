d212d3fcbb4e6e2c2e8d8cb441dc28714c07d0b3
Romain Trotard <romain.trotard@gmail.com>
2023-12-17 21:42:31 +0100

    Day14 part 2

--- day14/day14.go
+++ day14/day14.go
@@ -3,18 +3,23 @@ package main
 import (
 	"aoc/utils"
 	"fmt"
+	"strings"
 )
+
+var cache = map[string]int{}
 
 type Game struct {
 	board [][]string
 }
 
 func (game Game) String() string {
+	value := ""
+
 	for _, row := range game.board {
-		fmt.Println(row)
+		value += strings.Join(row, "")
 	}
 
-	return ""
+	return value
 }
 
 func (game Game) getNumberOfRows() int {
@@ -33,7 +38,7 @@ func isHardRock(value string) bool {
 	return value == "#"
 }
 
-func (game Game) tiltBoard() {
+func (game Game) tiltBoardNorth() {
 	for i := 1; i < game.getNumberOfRows(); i++ {
 		for j := 0; j < game.getNumberOfColumns(); j++ {
 			value := game.board[i][j]
@@ -54,6 +59,90 @@ func (game Game) tiltBoard() {
 					}
 
 					currentI = i - index
+					index++
+				}
+			}
+		}
+	}
+}
+
+func (game Game) tiltBoardWest() {
+	for j := 1; j < game.getNumberOfColumns(); j++ {
+		for i := 0; i < game.getNumberOfRows(); i++ {
+			value := game.board[i][j]
+
+			if isRoundedRock(value) {
+				// Let's move the rock as long as it's possible
+				index := 1
+				currentJ := j
+
+				for j-index >= 0 {
+					upperValue := game.board[i][j-index]
+
+					if upperValue == "." {
+						game.board[i][currentJ] = "."
+						game.board[i][j-index] = "O"
+					} else {
+						break
+					}
+
+					currentJ = j - index
+					index++
+				}
+			}
+		}
+	}
+}
+
+func (game Game) tiltBoardEast() {
+	for j := game.getNumberOfColumns() - 2; j >= 0; j-- {
+		for i := 0; i < game.getNumberOfRows(); i++ {
+			value := game.board[i][j]
+
+			if isRoundedRock(value) {
+				// Let's move the rock as long as it's possible
+				index := 1
+				currentJ := j
+
+				for j+index < game.getNumberOfColumns() {
+					upperValue := game.board[i][j+index]
+
+					if upperValue == "." {
+						game.board[i][currentJ] = "."
+						game.board[i][j+index] = "O"
+					} else {
+						break
+					}
+
+					currentJ = j + index
+					index++
+				}
+			}
+		}
+	}
+}
+
+func (game Game) tiltBoardSouth() {
+	for i := game.getNumberOfRows() - 2; i >= 0; i-- {
+		for j := 0; j < game.getNumberOfColumns(); j++ {
+			value := game.board[i][j]
+
+			if isRoundedRock(value) {
+				// Let's move the rock as long as it's possible
+				index := 1
+				currentI := i
+
+				for i+index < game.getNumberOfRows() {
+					upperValue := game.board[i+index][j]
+
+					if upperValue == "." {
+						game.board[currentI][j] = "."
+						game.board[i+index][j] = "O"
+					} else {
+						break
+					}
+
+					currentI = i + index
 					index++
 				}
 			}
@@ -99,7 +188,22 @@ func main() {
 		game.board = append(game.board, row)
 	})
 
-	game.tiltBoard()
+    // Find that pattern repeats each 143
+    // 1000000000 - 143*999999858 = 142
+	for i := 0; i < 142; i++ {
+		game.tiltBoardNorth()
+		game.tiltBoardWest()
+		game.tiltBoardSouth()
+		game.tiltBoardEast()
+
+		value, exist := cache[game.String()]
+
+		if exist {
+			fmt.Println("i", i, "in cache", value)
+		}
+
+		cache[game.String()] = i
+	}
 
 	fmt.Println("Result", game.getTotalLoad())
 }
