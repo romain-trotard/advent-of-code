eaf79861caa2f5b36e62c822c0b617abb8a0f918
Romain Trotard <romain.trotard@gmail.com>
2024-12-07 22:46:21 +0100

    Day6 part 2

--- day6/day6.ts
+++ day6/day6.ts
@@ -12,7 +12,7 @@ const USER = '^';
 const OBSTACLE = '#';
 const PASSED_BY = 'X';
 
-class Game {
+class InitGame {
     // All will be based 1
     // -> x
     // \ 
@@ -21,6 +21,8 @@ class Game {
     #board: Board;
     #currentUserPosition: Position;
     #currentDirection: Direction = 'up';
+
+    #positions: Position[] = [];
 
     constructor(lines: Array<string>) {
         this.#board = lines.map(line => {
@@ -104,61 +106,16 @@ class Game {
 
     }
 
-    #markCurrentCaseAsPassedBy() {
-        const previousLine = this.#board[this.#currentUserPosition.y - 1];
-        assertDefined(previousLine)
+    #addPosition() {
+        const present = this.#positions.some(position => position.x === this.#currentUserPosition.x && position.y === this.#currentUserPosition.y);
 
-        const currentValue = previousLine[this.#currentUserPosition.x - 1];
-        assertDefined(currentValue);
-
-        let newValue: string = currentValue;
-
-        if (currentValue === '.') {
-            switch (this.#currentDirection) {
-                case "up":
-                    newValue = '|'
-                    break;
-                case "down":
-                    newValue = '|'
-                    break;
-                case "right":
-                    newValue = '-'
-                    break;
-                case "left":
-                    newValue = '-'
-                    break;
-            }
-        } else {
-            switch (this.#currentDirection) {
-                case "up": {
-                    if (currentValue === '-') {
-                        newValue = '+';
-                    }
-                    break;
-                }
-                case "down": {
-                    if (currentValue === '-') {
-                        newValue = '+';
-                    }
-                    break;
-                }
-                case "right": {
-                    if (currentValue === '|') {
-                        newValue = '+';
-                    }
-                    break;
-                }
-                case "left": {
-                    if (currentValue === '|') {
-                        newValue = '+';
-                    }
-                    break;
-                }
-            }
+        if (present) {
+            return;
         }
 
-        assertDefined(newValue);
-        previousLine[this.#currentUserPosition.x - 1] = newValue;
+        this.#positions.push(
+            this.#currentUserPosition,
+        );
     }
 
     // As long as he can go in the current direction let's do this
@@ -167,8 +124,6 @@ class Game {
     * @return false when the user reaches the side
     */
     #moveUser(): boolean {
-        this.#markCurrentCaseAsPassedBy();
-
         const nextUserPosition = this.#getNextUserPositionWhenGoingForward();
         const { x, y } = nextUserPosition;
 
@@ -176,8 +131,7 @@ class Game {
             x === 0 || x > this.#columnNumber()
             || y === 0 || y > this.#rowNumber()
         ) {
-            // Mark the current case as passed by
-            // this.#markCurrentCaseAsPassedBy();
+            this.#addPosition();
             return false
         }
 
@@ -192,7 +146,7 @@ class Game {
             return true;
         }
 
-        // this.#markCurrentCaseAsPassedBy();
+        this.#addPosition();
 
         this.#currentUserPosition = nextUserPosition;
 
@@ -231,16 +185,179 @@ class Game {
 
         return result;
     }
+
+    getPositions() {
+        return this.#positions;
+    }
+}
+
+class Game {
+    // All will be based 1
+    // -> x
+    // \ 
+    // v 
+    // y
+    #board: Board;
+    #currentUserPosition: Position;
+    #currentDirection: Direction;
+
+    constructor(lines: Array<string>, initObstacle: Position, userInitPosition: Position) {
+        this.#board = lines.map(line => {
+            return line.split('');
+        });
+
+        const blockLine = this.#board[initObstacle.y - 1];
+        assertDefined(blockLine);
+        blockLine[initObstacle.x - 1] = OBSTACLE;
+
+        this.#currentUserPosition = userInitPosition;
+        this.#currentDirection = 'up';
+    }
+
+    #rowNumber(): number {
+        return this.#board.length;
+    }
+
+    #columnNumber(): number {
+        const line = this.#board[0]
+        assertDefined(line);
+
+        return line.length;
+    }
+
+    #turn(): Direction {
+        switch (this.#currentDirection) {
+            case "up":
+                return 'right';
+            case "down":
+                return 'left';
+            case "right":
+                return 'down';
+            case "left":
+                return 'up';
+        }
+    }
+
+    #getNextUserPositionWhenGoingForward(): Position {
+        const { x, y } = this.#currentUserPosition;
+        switch (this.#currentDirection) {
+            case "up":
+                return {
+                    x: x,
+                    y: y - 1,
+                }
+            case "down":
+                return {
+                    x: x,
+                    y: y + 1,
+                }
+            case "right":
+                return {
+                    x: x + 1,
+                    y: y,
+                }
+            case "left":
+                return {
+                    x: x - 1,
+                    y: y,
+                }
+        }
+
+    }
+
+    // As long as he can go in the current direction let's do this
+    // when meeting obstacle he turns right
+    /** 
+    * @return false when the user reaches the side
+    */
+    moveUser(): boolean {
+        const nextUserPosition = this.#getNextUserPositionWhenGoingForward();
+        const { x, y } = nextUserPosition;
+
+        if (
+            x === 0 || x > this.#columnNumber()
+            || y === 0 || y > this.#rowNumber()
+        ) {
+            return false
+        }
+
+        const line = this.#board[y - 1];
+        assertDefined(line)
+
+        const value = line[x - 1];
+        assertDefined(value);
+
+        if (value === OBSTACLE) {
+            this.#currentDirection = this.#turn();
+            return true;
+        }
+
+        this.#currentUserPosition = nextUserPosition;
+
+        return true;
+    }
+
+    move() {
+        let didMove = false;
+        do {
+            didMove = this.moveUser();
+        } while (didMove);
+    }
+
+    getUserPosition() {
+        return {
+            ...this.#currentUserPosition,
+            direction: this.#currentDirection,
+        }
+    }
 }
 
 async function main() {
     const lines = await getFileLines(`${__dirname}/input.txt`)
 
-    const game = new Game(lines);
+    const initGame = new InitGame(lines);
+    initGame.move();
 
-    game.move();
+    let result = 0;
 
-    const result = game.count();
+    // I use Floyd's cycle-finding algorithm
+    for (let index = 1; index < initGame.getPositions().length; index++) {
+        const userPosition = initGame.getPositions()[0];
+        assertDefined(userPosition);
+
+        const obstaclePosition = initGame.getPositions()[index];
+        assertDefined(obstaclePosition);
+
+        const slowGame = new Game(lines, obstaclePosition, userPosition);
+        const fastGame = new Game(lines, obstaclePosition, userPosition);
+
+        while (true) {
+            const slowMove = slowGame.moveUser();
+            if (!slowMove) {
+                break;
+            }
+
+            const fastMove = fastGame.moveUser();
+            if (!fastMove) {
+                break;
+            }
+            const secondFastMove = fastGame.moveUser();
+            if (!secondFastMove) {
+                break;
+            }
+
+            const { x: slowX, y: slowY, direction: slowDirection } = slowGame.getUserPosition();
+            const { x: fastX, y: fastY, direction: fastDirection } = fastGame.getUserPosition();
+
+            if (slowX === fastX && slowY === fastY && slowDirection === fastDirection) {
+                result += 1;
+                // console.log({ obstaclePosition, userPosition, slowX, slowY, slowDirection });
+                console.log({ obstaclePosition });
+                break;
+            }
+        }
+
+    }
 
     console.log('The result is', result);
 }
