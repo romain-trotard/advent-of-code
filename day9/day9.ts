d8b993ba113edc1a0924690798958aa7a9cb1301
Romain Trotard <romain.trotard@gmail.com>
2024-12-11 21:25:31 +0100

    Day9 part 2

--- day9/day9.ts
+++ day9/day9.ts
@@ -4,23 +4,11 @@ import { getFileLines } from "../utils/fileUtils";
 type File = {
     fileSpace: number;
     freeSpace: number;
+    value: number;
+    processed?: boolean;
 }
 
 const FREE_SPACE = ' ';
-
-function buildFileIndexed(files: Array<File>): Array<number | typeof FREE_SPACE> {
-    const flatArray: Array<number | typeof FREE_SPACE> = [];
-
-    for (let i = 0; i < files.length; i++) {
-        const file = files[i];
-        assertDefined(file);
-
-        flatArray.push(...new Array(file.fileSpace).fill(i));
-        flatArray.push(...new Array(file.freeSpace).fill(FREE_SPACE));
-    }
-
-    return flatArray;
-}
 
 async function main() {
     const lines = await getFileLines(`${__dirname}/input.txt`)
@@ -31,45 +19,70 @@ async function main() {
 
     const numbers = firstLineAndOnlyOne.split('').map(value => Number.parseInt(value, 10));
 
-    let currentFile: File = {
-        fileSpace: 0,
-        freeSpace: 0,
-    }
+    let currentFile: File | null = null;
 
     numbers.forEach((value, index) => {
         if ((index + 1) % 2 === 0) {
+            assertDefined(currentFile);
             currentFile.freeSpace = value;
         } else {
-            currentFile = { fileSpace: value, freeSpace: 0 };
+            currentFile = { fileSpace: value, freeSpace: 0, value: Math.floor(index / 2) };
             files.push(currentFile)
         }
     });
 
 
-    // Create intermediate flatten array
-    const processingArray = buildFileIndexed(files);
-    let continueProcessing = processingArray.includes(FREE_SPACE);
+    let processingFiles = files.toReversed();
 
-    while (continueProcessing) {
-        const lastValue = processingArray.pop();
-        assertDefined(lastValue);
+    for (let i = 0; i < processingFiles.length; i++) {
+        const processingFile = processingFiles[i];
+        assertDefined(processingFile);
 
-        // If the last value is a space just remove it and continue
-        if (lastValue === FREE_SPACE) {
+        // We want to get the first file that has enough space for the file being process but in the right order so we reverse it
+        const orderedFilesToProcess = processingFiles.toReversed()
+        const fileWithFreeSpaceIndex = orderedFilesToProcess.findIndex(file => file.freeSpace >= processingFile.fileSpace);
+        const orderedIndexInReversedBase = processingFiles.length - 1 - fileWithFreeSpaceIndex;
+
+        if (fileWithFreeSpaceIndex === -1 || orderedIndexInReversedBase <= i) {
             continue;
         }
 
-        const freeSpaceIndex = processingArray.indexOf(FREE_SPACE);
-        processingArray[freeSpaceIndex] = lastValue;
+        const fileWithFreeSpace = processingFiles[orderedIndexInReversedBase];
+        assertDefined(fileWithFreeSpace);
 
-        continueProcessing = processingArray.includes(FREE_SPACE)
+        // We need to keep in memory freeSpace of the file we gonna take freeSpace to change freeSpace of the file being processed
+        const tempFreeSpace = fileWithFreeSpace.freeSpace;
+        fileWithFreeSpace.freeSpace = 0
+
+        const tempProcessingFileFreeSpace = processingFile.freeSpace;
+        processingFile.freeSpace = tempFreeSpace - processingFile.fileSpace;
+
+        // We change the processing so that it does not take fileSpace anymore and replace it with free space
+        processingFiles[i] = { ...processingFile, freeSpace: tempProcessingFileFreeSpace + processingFile.fileSpace, fileSpace: 0 };
+        
+        processingFiles = processingFiles.flatMap((file, index) => {
+            if (index === orderedIndexInReversedBase) {
+                return [processingFile, file];
+            }
+
+            return file;
+        });
+    }
+
+    const flatArray: Array<number | typeof FREE_SPACE> = [];
+
+    for (const file of processingFiles.toReversed()) {
+        flatArray.push(...new Array(file.fileSpace).fill(file.value));
+        flatArray.push(...new Array(file.freeSpace).fill(FREE_SPACE));
     }
 
     let result = 0;
 
-    processingArray.filter(value => value !== FREE_SPACE)
+    flatArray
         .forEach((value, index) => {
-            result += value * index;
+            if (value !== FREE_SPACE) {
+                result += value * index;
+            }
         });
 
     console.log('The result is', result);
