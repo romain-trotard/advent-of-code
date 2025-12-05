56c6a8da115f479afb8e90bf2b0165420e9a85da
Romain Trotard <romain.trotard@gmail.com>
2025-12-05 16:04:11 +0100

    Day 4 part 1

---
+++ day4/day4.ts
@@ -0,0 +1,72 @@
+import { assertDefined } from "../utils/asserts";
+import { getFileLines } from "../utils/fileUtils";
+
+const ROLL = '@';
+
+async function main() {
+    const lines = await getFileLines(`${__dirname}/input.txt`)
+
+    const firstRow = lines.at(0);
+    assertDefined(firstRow);
+
+    const width = firstRow.length;
+    const height = lines.length;
+
+    const table = lines.map(line => line.split(''));
+    const processedTable: typeof table = table.map(() => []);
+
+    for (let j = 0; j < height; j++) {
+        for (let i = 0; i < width; i++) {
+            const row = table[j];
+            assertDefined(row);
+
+            const currentValue = row[i];
+            assertDefined(currentValue);
+
+            if (currentValue !== ROLL) {
+                const processedRow = processedTable[j];
+                assertDefined(processedRow);
+                processedRow[i] = currentValue;
+                continue;
+            }
+
+            const startingJ = j === 0 ? 0 : -1;
+            const endingJ = j === height - 1 ? 0 : 1;
+
+            const startingI = i === 0 ? 0 : -1;
+            const endingI = i === width - 1 ? 0 : 1;
+
+            let count = 0;
+
+            // It's a roll we to count everywhere aside the roll there are
+            for (let j2 = startingJ; j2 <= endingJ; j2++) {
+                for (let i2 = startingI; i2 <= endingI; i2++) {
+                    if (j2 === 0 && i2 === 0) {
+                        continue;
+                    }
+
+                    // I just can't do it anymore
+                    if (table[j + j2]![i + i2] === ROLL) {
+                        count++;
+                    }
+                }
+            }
+
+            const processedRow = processedTable[j];
+            assertDefined(processedRow);
+            if (count >= 4) {
+                processedRow[i] = 'x';
+            } else {
+                processedRow[i] = '@';
+            }
+        }
+    }
+
+    const result = processedTable.map(line => line.filter(v => v === ROLL).length).reduce((a, b) => a + b);
+
+    console.log('The result is', result);
+}
+
+main();
+
+
